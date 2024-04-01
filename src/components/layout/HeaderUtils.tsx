d05df8db3548755c63d5601686f20412bfd80f7b
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-04-01 12:48:56 +0330

    Add badge to cart icon

--- src/components/layout/HeaderUtils.tsx
+++ src/components/layout/HeaderUtils.tsx
@@ -3,10 +3,26 @@ import { FC } from "react";
 
 const HeaderUtils: FC = () => {
   return (
-    <div className="flex gap-x-5">
-      <Image src="/icons/huge-icon/interface/solid/search 01.svg" alt="" width="24" height="24" />
-      <Image src="/icons/huge-icon/ecommerce/outline/favourite.svg" alt="" width="24" height="24" />
-      <Image src="/icons/huge-icon/ecommerce/outline/cart.svg" alt="" width="24" height="24" />
+    <div className="flex gap-x-5 items-center">
+      <div>
+        <Image src="/icons/huge-icon/interface/solid/search 01.svg" alt="" width="24" height="24" />
+      </div>
+      <div>
+        <Image
+          src="/icons/huge-icon/ecommerce/outline/favourite.svg"
+          alt=""
+          width="24"
+          height="24"
+        />
+      </div>
+      <div className="relative">
+        <Image src="/icons/huge-icon/ecommerce/outline/cart.svg" alt="" width="24" height="24" />
+        <div className="size-4 absolute -top-2 -right-2 rounded-full bg-primary-500 z-[1] flex items-center justify-center p-2.5">
+          <span className="font-bold text-white text-sm">
+            2
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
