3a11e3fc9000ebc4c11f53efe5cacd8dfa02ac3c
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-04-02 01:37:23 +0330

    Fix hero section postionings

--- src/components/homepage/MainHero.tsx
+++ src/components/homepage/MainHero.tsx
@@ -6,18 +6,18 @@ import Button from "../common/Button";
 
 const MainHero: FC = () => {
   return (
-    <div className="bg-objects-group bg-contain w-full h-screen relative bg-gray-500 bg-opacity-10">
-      <div className="relative bg-yellow-50">
+    <div className="relative bg-objects-group bg-contain w-full h-[calc(100vh-166px)] bg-gray-500 bg-opacity-10">
+      <div className="relative h-full">
         {/* White background circle */}
         <div className="absolute size-[30vw] bg-white rounded-full top-20 right-24"></div>
 
         {/* Headphone guy */}
-        <div className="absolute right-16 top-0">
-          <Image src={HeadphoneDude} className="w-[30vw]" alt="" />
+        <div className="absolute right-16 top-0 bottom-0 overflow-hidden">
+          <Image src={HeadphoneDude} className="h-full" alt="" />
         </div>
       </div>
 
-      <div>
+      <div className="absolute top-1/2 left-40 -translate-y-1/2">
         <h2 className="text-7xl font-bold text-dark-500 leading-snug">
           Great Sound
           <br />
@@ -25,7 +25,8 @@ const MainHero: FC = () => {
           <br />
           Headphone
         </h2>
-        <p className="text-dark-500">
+
+        <p className="text-dark-500 mt-4 mb-8 leading-normal">
           It is a long established fact that a reader will be
           <br /> distracted by the readable content of a page when
           <br /> looking at its layout. The point of using Lorem Ipsum.
