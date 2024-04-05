b127d7aef963fbb7a46fd2d68786d5451eb34aa7
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-04-05 15:30:47 +0330

    Add montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header/Header";
 import Navigation from "@/components/layout/header/Navigation";
 import Footer from "@/components/layout/footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Next App",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <Header />
         <Navigation />
         {children}
