b1b6fc20f1bca3aacc79675b53c26e1f642ba923
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-05-29 16:51:28 +0330

    Add discount model

--- backend/onlineshop/products/models.py
+++ backend/onlineshop/products/models.py
@@ -5,4 +5,13 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=520)
     price = models.FloatField()
-    
+
+
+class Discount(models.Model):
+    class DiscountType(models.TextChoices):
+        VALUE = "VAL"
+        PERCENTAGE = "PRCNT"
+
+    code = models.CharField(max_length=8)
+    type = models.CharField(max_length=5, choices=DiscountType, default=DiscountType.VALUE)
+    value = models.FloatField()
