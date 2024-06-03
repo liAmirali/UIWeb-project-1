7fc91387ed3ffe7bd52d4da0cdd7641735cfa26b
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-04 00:29:12 +0330

    Add basic store models

--- backend/onlineshop/products/models.py
+++ backend/onlineshop/products/models.py
@@ -1,17 +1,34 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from user.models import User
+
+
+class Discount(models.Model):
+    class DiscountType(models.TextChoices):
+        VALUE = "VALUE"
+        PERCENTAGE = "PERCENTAGE"
+
+    code = models.CharField(max_length=8)
+    type = models.CharField(
+        max_length=5, choices=DiscountType, default=DiscountType.VALUE)
+    value = models.FloatField()
+    max_price_limit = models.FloatField(validators=[MinValueValidator(0)])
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User)
+    discount = models.ForeignKey(Discount, null=True)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=520)
-    price = models.FloatField()
+    rating = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
 
 
-class Discount(models.Model):
-    class DiscountType(models.TextChoices):
-        VALUE = "VAL"
-        PERCENTAGE = "PRCNT"
-
-    code = models.CharField(max_length=8)
-    type = models.CharField(max_length=5, choices=DiscountType, default=DiscountType.VALUE)
-    value = models.FloatField()
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart)
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    product = models.ForeignKey(Product)
