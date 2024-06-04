25b97cc28c09ac495866081302c0c26ec8d46083
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-04 10:39:03 +0330

    Register models on admin site

--- backend/onlineshop/products/admin.py
+++ backend/onlineshop/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Category, Color, Product, ProductPriceHistory, Discount
 
-# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Category)
+admin.site.register(Color)
+admin.site.register(Product)
+admin.site.register(ProductPriceHistory)
+admin.site.register(Discount)
