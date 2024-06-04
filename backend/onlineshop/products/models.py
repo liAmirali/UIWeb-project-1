61e6c2c233d9971ea0070b11810b6a60a022d0af
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-04 10:34:35 +0330

    Add product models

--- backend/onlineshop/products/models.py
+++ backend/onlineshop/products/models.py
@@ -4,6 +4,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from user.models import User
 
 
+class Cart(models.Model):
+    discount = models.ForeignKey(
+        'Discount', on_delete=models.SET_NULL, null=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+
 class Discount(models.Model):
     class DiscountType(models.TextChoices):
         VALUE = "VALUE"
@@ -11,24 +23,41 @@ class Discount(models.Model):
 
     code = models.CharField(max_length=8)
     type = models.CharField(
-        max_length=5, choices=DiscountType, default=DiscountType.VALUE)
+        max_length=10, choices=DiscountType, default=DiscountType.VALUE)
     value = models.FloatField()
-    max_price_limit = models.FloatField(validators=[MinValueValidator(0)])
+    max_price_limit = models.FloatField(validators=[MinValueValidator(0)], null=True)
 
-
-class Cart(models.Model):
-    user = models.ForeignKey(User)
-    discount = models.ForeignKey(Discount, null=True)
+    applicable_products = models.ManyToManyField('Product')
+    applicable_categories = models.ManyToManyField('Category')
 
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=520)
     rating = models.FloatField(
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
+        validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+
+    def get_latest_price(self):
+        latest_price_entry = self.prices.order_by('-date').first()
+        return latest_price_entry.price if latest_price_entry else None
 
 
-class CartItem(models.Model):
-    cart = models.ForeignKey(Cart)
-    quantity = models.IntegerField(validators=[MinValueValidator(0)])
-    product = models.ForeignKey(Product)
+class ProductPriceHistory(models.Model):
+    date = models.DateTimeField(auto_now_add=True, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[
+                                MinValueValidator(0)])
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="prices")
+
+
+class Color (models.Model):
+    name = models.CharField(max_length=50)
+    hex_code = models.CharField(max_length=7)
+    extra_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='colors')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    subcategories = models.ForeignKey('Category', on_delete=models.CASCADE)
--- backend/onlineshop/user/models.py
+++ backend/onlineshop/user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from products.models import Cart
 
 class User(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    cart = models.OneToOneField('products.Cart', on_delete=models.SET_NULL, null=True)
