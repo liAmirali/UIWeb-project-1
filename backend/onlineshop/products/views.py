673fd78c6b99330db41ee732f2e2b1f0f2e88cb6
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-08 12:35:55 +0330

    Add permissoins to product view

--- backend/onlineshop/products/views.py
+++ backend/onlineshop/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -6,3 +6,4 @@ from .models import Product
 class ProductListView(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
