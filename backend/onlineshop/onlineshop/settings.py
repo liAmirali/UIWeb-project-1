21e9e27492527a8833098ba441ae519252175241
Amirali Lotfi <liamirali.lotfi@gmail.com>
2024-06-13 17:40:08 +0330

    Fix CORS error

--- backend/onlineshop/onlineshop/settings.py
+++ backend/onlineshop/onlineshop/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'user',
     'store'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
+
+# CORS Settings
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
